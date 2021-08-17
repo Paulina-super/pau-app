1b2769e44af7b7687820f9c4b5328a9412b4df02
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-08-16 23:15:48 -0300

    0000

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import NavBar from 'NavBar';
+import NavBar from './components/NavBar/NavBar.js';
 import './App.css';
-import CartWidget from './components/CartWidget/CartWidget';
+import CartWidget from './src/components/CartWidget/CartWidget.js';
 
 
 function App() {
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FaSolidFaCartShopping } from '@fortawesome/react-fontawesome-svg-core';
+
 
 
 
