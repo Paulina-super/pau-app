d659e81b2a9e6d3e65570547e6289f9422fbda3b
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-08-16 22:42:31 -0300

    Recuperando

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { MenuItems } from "./MenuItems"
+import "./NavBar.css"
+import "./CartWidget/CartWidget"
+
+class NavBar extends React.Component {
+ state={clicked: false}
+
+ handleClick=()=>{
+     this.setState({ clicked: !this.state.clecked })
+ }
+
+    render(){
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">Samuel<i className="fas fa-cat"></i></h1>
+                
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times': 'fas fa-bars'}></i>
+                    
+                </div>
+                <ul className={this.state.clicked ?'nav-menu active':'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return(
+                            <li key={index}>
+                                <a className={MenuItems.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default NavBar
