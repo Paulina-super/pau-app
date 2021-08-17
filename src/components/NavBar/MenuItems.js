d3dbfa863ef08bad69c4d006ac44d7e59d0badb8
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-08-16 22:45:57 -0300

    ++

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,26 @@
+export const MenuItems=[
+    {
+        title:'Home',
+        url:'#',
+        cName:'nav-links'
+    
+    },
+    {
+        title:'Productos',
+        url:'#',
+        cName:'nav-links'
+        
+    },
+    {
+        title:'Servicios',
+        url:'#',
+        cName:'nav-links'
+            
+    },
+    {
+        title:'Contacto',
+        url:'#',
+        cName:'nav-links'
+                
+    }
+]
