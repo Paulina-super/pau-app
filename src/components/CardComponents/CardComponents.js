fd0c8fa84c91487691b4fa69367ccbde5cad3902
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-08-19 07:04:46 -0300

    321

--- src/components/CardComponents/CardComponents.js
+++ src/components/CardComponents/CardComponents.js
@@ -8,13 +8,12 @@ const extra = (
   </a>
 )
 
-const CardComponents = () => (
+const CardComponents = ({image , name , description, button}) => (
   <Card
     image='/images/avatar/large/elliot.jpg'
-    header='Elliot Baker'
-    meta='Friend'
-    description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-    extra={extra}
+    name='Food'
+    description='Cat'
+    button={extra}
   />
 )
 
