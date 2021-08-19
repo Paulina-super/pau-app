32a1d43de2287ec125b18ae6566199a07622d3dd
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-08-19 06:54:14 -0300

    Card

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import ItemListContainer from './components/NavBar/ItemListContainer';
+import CardComponents from './components/CardComponents/CardComponents';
 
 function App() {
   return (
     <div className="App">
       <Header title='Samuel App'
       subtitle='Miau'/>
-      <ItemListContainer greeting='¡Bienvenidos a Tienda Samuel!'/>         
+      <ItemListContainer greeting='¡Bienvenidos a Tienda Samuel!'/>   
+      <CardComponents />      
     </div>
   );
 }
