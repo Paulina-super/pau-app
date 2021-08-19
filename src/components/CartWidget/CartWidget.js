ff21ff60feb1b5f2590ab6b9cf416fe890116b68
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-08-19 20:08:04 -0300

    modificacion entrega

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,19 +2,12 @@ import React from 'react';
 import './CartWidget.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
-
-
-
 const CartWidget = () => {
     return (
         <div className='carrito'>
-           <FontAwesomeIcon icon="fa-solid fa-cart-arrow-down" />
-           
+           <FontAwesomeIcon icon={faCartArrowDown} />
         </div>
-
-
     );
 };
-
 
 export default CartWidget
