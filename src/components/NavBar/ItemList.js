638edd26eddee92d9678049d8c6e5e371b26d2ed
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-08-31 02:52:29 -0300

    :D

--- src/components/NavBar/ItemList.js
+++ src/components/NavBar/ItemList.js
@@ -1,14 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import Item from '../NavBar/Item.js';
+import axios from 'axios';
 
 const ItemList = () => {
     const[products,setProducts] = useState([]);
     
     
     useEffect(()=>{
-        fetch('https://fakestoreapi.com/products')
-            .then(response => response.json())
-            .then(json => setProducts(json));
+        axios('https://fakestoreapi.com/products')
+            .then((res) => setProducts(res.data));
+            
     }, []);
 
     return (
