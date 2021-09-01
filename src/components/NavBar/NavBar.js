60cf5a49ff10c6118a67f3420caa68eab1004b45
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-09-01 20:14:15 -0300

    ****

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -17,10 +17,10 @@ function NavBar (){
                         {MenuItems.map((item, index) => {
                             return(
                                 <li key={index}>
-                                    <link to='/'><a className={MenuItems.cName} href={item.url}>
+                                    <Link to='/'><a className={MenuItems.cName} href={item.url}>
                                                     {item.title}
                                                 </a>
-                                    </link>
+                                    </Link>
                                 </li>                            
                             )
                         })}
