75c98434a6fc2940a6cc98cb3bd0481f2ceeb7ea
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-10-06 21:34:32 -0300

    Mas ajustes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Header.css';
 import NavBar from '../NavBar/NavBar.js';
+import {NavLink} from 'react-router-dom';
 
 
 const Header = ({title, subtitle}) => {
     return (
         <div className='Header'>
             
-            <h1>{title}</h1>
-            <h2>{subtitle}</h2>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/Catalogo">Catalogo</NavLink>
+            <NavLink to="/Us">Us</NavLink>
+            <NavLink to="/Contacto">Contacto</NavLink>
                        
             <NavBar />
             
